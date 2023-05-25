3ca4623dd3155f7560ab5a79602744cb3f12ecad
PramudyaReynaldi <prmdy02@gmail.com>
2023-05-25 14:18:16 +0700

    first commit

---
+++ routes.js
@@ -0,0 +1,74 @@
+// Method/Verb Request dan Routing
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+
+  // Merhod '*' ini mendandakan bahwa ada method selain yang didefisikan
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Tidak ada method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Tidak ada method tersebut";
+    },
+  },
+
+  // Path parameters
+  {
+    method: "GET",
+    path: "/users/{username}",
+    handler: (request, h) => {
+      const { username } = request.params;
+      return `Hello, ${username}`;
+    },
+  },
+
+  // Path parameter using ?
+  {
+    method: "GET",
+    path: "/user/{nama?}",
+    handler: (request, h) => {
+      const { nama = "stranger" } = request.params;
+      return `Hello, ${nama}`;
+    },
+  },
+
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      return `Hello, ${name}`;
+    },
+  },
+
+  // Query parameters
+  {
+    method: "GET",
+    path: "/query", // Saya memakai path query karena sudah kehabisan path wkwk
+    handler: (request, h) => {
+      const { pengguna, lokasi } = request.query; // Contoh Query parameter
+      return `Halo ${pengguna}, kita berada di ${lokasi}`;
+    },
+  },
+];
+
+module.exports = routes;
